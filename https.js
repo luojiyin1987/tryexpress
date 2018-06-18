dcfd433a8a291545f47d9299d75de29bb72fda8f
luojiyin <luojiyin@hotmail.com>
2018-06-18 11:49:20 +0800

    feat: test 12306

--- https.js
+++ https.js
@@ -1,12 +1,9 @@
 var https = require('https')
 
-https.get('https://www.baidu.com', function(res){
-  console.log('status code: '+ res.statusCode);
-  console.log('headers: '+ res.headers);
-
+https.get('https://kyfw.12306.cn/otn/regist/init', function(res){
   res.on('data', function(data){
     process.stdout.write(data);
-  })
+  });
 }).on('error', function(err){
   console.error(err);
 });
